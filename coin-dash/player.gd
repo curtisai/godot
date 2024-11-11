e3caf509549a217dd695e16edc4f86832b839449
Xiaolong Zhang <notadish@gmail.com>
2024-11-12 00:33:45 +0800

    added script

--- coin-dash/player.gd
+++ coin-dash/player.gd
@@ -1,5 +1,8 @@
 extends Area2D
 
+@export var speed:int
+var velocity = Vector2()
+var screensize = Vector2(720, 720)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +11,21 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	get_input()	
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("ui_accept"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	print_debug(velocity)

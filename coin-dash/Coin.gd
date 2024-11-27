503542eaedebbf0ccc53a179f2ce344aacec02c3
Xiaolong Zhang <notadish@gmail.com>
2024-11-27 22:48:26 +0800

    befre tween

--- coin-dash/Coin.gd
+++ coin-dash/Coin.gd
@@ -10,6 +10,7 @@ extends Area2D
 func _ready():
 	pass # Replace with function body.
 
+var screensize = Vector2()
 
 func pickup():
 	queue_free()
--- coin-dash/Main.gd
+++ coin-dash/Main.gd
@@ -1,10 +1,8 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+# In the $ notation, the node name is relative to the node running
+# to the script. For example, $Node1/Node2 would refer to
+# a node (Node2) that is the child of Node1
 export (PackedScene) var Coin
 export (int) var playtime
 
@@ -14,15 +12,59 @@ var time_left
 var screensize
 var playing = false
 
+func new_game():
+	playing = true
+	level = 1
+	score = 0
+	time_left = playtime
+	$Player.start($PlayerStart.position)
+	$Player.show()
+	$GameTimer.start()
+	spawn_coins()
+	$HUD.update_score(score)
+	$HUD.update_timer(time_left)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
 	screensize = get_viewport().get_visible_rect().size
 	$Player.screensize = screensize
 	$Player.hide()
-	# Replace with function body.
 
 
+func spawn_coins():
+	for i in range(4 + level):
+		var c = Coin.instance()
+		$CoinContainer.add_child(c)
+		c.screensize = screensize
+		c.position = Vector2(rand_range(0, screensize.x),
+		rand_range(0, screensize.y))
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if playing and $CoinContainer.get_child_count() == 0:
+		level += 1
+		time_left += 5
+		spawn_coins()
+
+func _on_GameTimer_timeout():
+	time_left -= 1
+	$HUD.update_timer(time_left)
+	if time_left <= 0:
+		game_over()
+
+func game_over():
+	playing = false
+	$GameTimer.stop()
+	for coin in $CoinContainer.get_children():
+		coin.queue_free()
+	$HUD.show_game_over()
+	$Player.die()
+
+
+func _on_Player_hurt():
+	game_over()
+
+
+func _on_Player_pickup():
+	score += 1
+	$HUD.update_score(score)
